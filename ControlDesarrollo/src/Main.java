116e1257af9002c41f236218e174498aefad5683
Miguel <Miguel@DESKTOP-0MUMB30>
2022-12-07 20:43:34 +0100

    V4, se suma una linea desde eclipse

--- ControlDesarrollo/src/Main.java
+++ ControlDesarrollo/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hola me llamo"); 
+		System.out.println("Hola me llamo");
+		System.out.println("Hasta luego"); 
 	}
 }
